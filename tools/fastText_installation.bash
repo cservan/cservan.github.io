6913e5d4189e846ad868455160b717a364e6df42
Christophe SERVAN <c.servan@qwant.com>
2020-07-07 19:06:51 +0200

    .

--- tools/fastText_installation.bash
+++ tools/fastText_installation.bash
@@ -1,14 +1,11 @@
-DESTINATION=/$HOME/Tools/fastText
+export DESTINATION=/$HOME/Tools/fastText
 
 rm -rf $DESTINATION
 mkdir -pv /$HOME/Tools/
 
-source ~/.bashrc
-
-pushd $DESTINATION
+pushd /$HOME/Tools/
 	git clone https://github.com/facebookresearch/fastText.git
     cd fastText && mkdir build && cd build && cmake .. && make
     sudo make install
 popd
 
-source ~/.bashrc
