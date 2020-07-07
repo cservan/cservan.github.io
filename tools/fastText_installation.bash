21b0035fa6b75e615030e25a134f51e7a21ba46e
Christophe SERVAN <c.servan@qwant.com>
2020-07-07 19:10:01 +0200

    updated fastText_installation

--- tools/fastText_installation.bash
+++ tools/fastText_installation.bash
@@ -1,11 +1,16 @@
-export DESTINATION=/$HOME/Tools/fastText
+DESTINATION=/$HOME/Tools/fastText/
 
 rm -rf $DESTINATION
-mkdir -pv /$HOME/Tools/
+mkdir -pv $DESTINATION
 
-pushd /$HOME/Tools/
-	git clone https://github.com/facebookresearch/fastText.git
-    cd fastText && mkdir build && cd build && cmake .. && make
-    sudo make install
+echo "export PATH=\$PATH:$DESTINATION/bin" >> ~/.bashrc
+
+source ~/.bashrc
+
+pushd $DESTINATION
+	wget https://github.com/cservan/website/raw/master/tools/fastText.tar.gz
+	tar xvfz fastText.tar.gz
+    cd fastText && mkdir build && cd build && cmake .. -DCMAKE_INSTALL_PREFIX=$DESTINATION && make -j && make install
 popd
 
+source ~/.bashrc
